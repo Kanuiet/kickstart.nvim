16f52c2f3e0c833188f7adfd61bb162d77ee4908
Kanuiet <Azhim4444@gmail.com>
2026-06-23 14:54:14 +0800

    remove go debugger

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -12,7 +12,6 @@ vim.pack.add {
   'https://github.com/nvim-neotest/nvim-nio',
   'https://github.com/mason-org/mason.nvim',
   'https://github.com/jay-babu/mason-nvim-dap.nvim',
-  'https://github.com/leoluz/nvim-dap-go',
 }
 
 -- Basic debugging keymaps, feel free to change to your liking!
@@ -41,7 +40,6 @@ require('mason-nvim-dap').setup {
   -- online, please don't ask me how to install them :)
   ensure_installed = {
     -- Update this to ensure that you have the debuggers for the langs you want
-    'delve',
   },
 }
 
@@ -84,12 +82,3 @@ dapui.setup {
 dap.listeners.after.event_initialized['dapui_config'] = dapui.open
 dap.listeners.before.event_terminated['dapui_config'] = dapui.close
 dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
--- Install golang specific config
--- require('dap-go').setup {
---   delve = {
---     -- On Windows delve must be run attached or it crashes.
---     -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
---     detached = vim.fn.has 'win32' == 0,
---   },
--- }
